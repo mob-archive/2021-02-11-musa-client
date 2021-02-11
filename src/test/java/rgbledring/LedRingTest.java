5239d71c9858330bac87db55aa8a44e480542214
Tobse <Tobse4Git@gmail.com>
2021-02-11 19:41:34 +0100

    mob next [ci-skip]

--- src/test/java/rgbledring/LedRingTest.java
+++ src/test/java/rgbledring/LedRingTest.java
@@ -1,84 +1,98 @@
 package rgbledring;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class LedRingTest {
 
-	private boolean[] leds;
-	private LedRing ledRing;
+    private boolean[] leds;
+    private LedRing ledRing;
 
-	@Test
-	void testGivenRingOfSize2_whenLevelIsSetTo51_thenLED1And2AreOn() {
-		givenLEDRingOfSize(2);
-		whenLevelIsSetTo(51);
-		thenLEDsAre(true, true);
-	}
+    @Test
+    void testGivenRingOfSize2_whenLevelIsSetTo51_thenLED1And2AreOn() {
+        givenLEDRingOfSize(2);
+        whenLevelIsSetTo(51);
+        thenLEDsAre(true, true);
+    }
 
-	@Test
-	void testGivenRingOfSize2_whenLevelIsSetTo0_thenLED1And2AreOff() {
-		givenLEDRingOfSize(2);
-		whenLevelIsSetTo(0);
-		thenLEDsAre(false, false);
-	}
+    @Test
+    void testGivenRingOfSize2_whenLevelIsSetTo0_thenLED1And2AreOff() {
+        givenLEDRingOfSize(2);
+        whenLevelIsSetTo(0);
+        thenLEDsAre(false, false);
+    }
 
-	@Test
-	void testGivenRingOfSize2_whenLevelIsSetTo2_thenLED1OnAndLed2Off() {
-		givenLEDRingOfSize(2);
-		whenLevelIsSetTo(2);
-		thenLEDsAre(true, false);
-	}
+    @Test
+    void testGivenRingOfSize2_whenLevelIsSetTo2_thenLED1OnAndLed2Off() {
+        givenLEDRingOfSize(2);
+        whenLevelIsSetTo(2);
+        thenLEDsAre(true, false);
+    }
 
-	@Test
-	void testGivenRingOfSize4_whenLevelIsSetTo0_thenAllLEDsAreOff() {
-		givenLEDRingOfSize(4);
-		whenLevelIsSetTo(0);
-		thenLEDsAre(false, false, false, false);
-	}
+    @Test
+    void testGivenRingOfSize4_whenLevelIsSetTo0_thenAllLEDsAreOff() {
+        givenLEDRingOfSize(4);
+        whenLevelIsSetTo(0);
+        thenLEDsAre(false, false, false, false);
+    }
 
-	@Test
-	void testGivenRingOfSize4_whenLevelIsSetTo51_thenOnlyLED4IsOff() {
-		givenLEDRingOfSize(4);
-		whenLevelIsSetTo(51);
-		thenLEDsAre(true, true, true, false);
-	}
+    @DisplayName("test Given Ring of size 16 when Level is set to -> AllLEDsAre Off")
+    @Test
+    void testGivenRingOfSize16_whenLevelIsSetTo0_thenAllLEDsAreOff() {
+        givenLEDRingOfSize(16);
+        whenLevelIsSetTo(0);
+        thenLEDsAre(false, false, false, false,
+                false, false, false, false,
+                false, false, false, false,
+                false, false, false, false
+        );
+    }
 
-	@Test
-	void testGivenRingOfSize4_whenLevelIsSetTo100_thenAllLED4AreOn() {
-		givenLEDRingOfSize(4);
-		whenLevelIsSetTo(100);
-		thenLEDsAre(true, true, true, true);
-	}
+    @Test
+    void testGivenRingOfSize4_whenLevelIsSetTo51_thenOnlyLED4IsOff() {
+        givenLEDRingOfSize(4);
+        whenLevelIsSetTo(51);
+        thenLEDsAre(true, true, true, false);
+    }
 
-	@Test
-	void testGivenRingOfSize4_whenLevelIsSetTo100Then0_thenAllLEDsAreOff() {
-		givenLEDRingOfSize(4);
-		whenLevelIsSetTo(100);
-		whenLevelIsSetTo(0);
-		thenLEDsAre(false, false, false, false);
-	}
+    @Test
+    void testGivenRingOfSize4_whenLevelIsSetTo100_thenAllLED4AreOn() {
+        givenLEDRingOfSize(4);
+        whenLevelIsSetTo(100);
+        thenLEDsAre(true, true, true, true);
+    }
 
-	@Test
-	void testGivenRingOfSize4_whenLevelIsSetTo100Then51_thenOnlyLED4AreOff() {
-		givenLEDRingOfSize(4);
-		whenLevelIsSetTo(100);
-		whenLevelIsSetTo(51);
-		thenLEDsAre(true, true, true, false);
-	}
+    @Test
+    void testGivenRingOfSize4_whenLevelIsSetTo100Then0_thenAllLEDsAreOff() {
+        givenLEDRingOfSize(4);
+        whenLevelIsSetTo(100);
+        whenLevelIsSetTo(0);
+        thenLEDsAre(false, false, false, false);
+    }
 
-	private void thenLEDsAre(boolean... expected) {
-		assertThat(leds).isEqualTo(expected);
-	}
+    @Test
+    void testGivenRingOfSize4_whenLevelIsSetTo100Then51_thenOnlyLED4AreOff() {
+        givenLEDRingOfSize(4);
+        whenLevelIsSetTo(100);
+        whenLevelIsSetTo(51);
+        thenLEDsAre(true, true, true, false);
+    }
 
-	private void givenLEDRingOfSize(int size) {
-		leds = new boolean[size];
-		ledRing = new LedRing(leds);
+    private void thenLEDsAre(boolean... expected) {
+        assertThat(leds).isEqualTo(expected);
+    }
 
-	}
 
-	private void whenLevelIsSetTo(int level) {
-		ledRing.setLevel(level);
-	}
+    private void givenLEDRingOfSize(int size) {
+        leds = new boolean[size];
+        ledRing = new LedRing(leds);
+
+    }
+
+    private void whenLevelIsSetTo(int level) {
+        ledRing.setLevel(level);
+    }
 
 }
